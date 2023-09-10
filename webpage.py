e8bf05d3d31bd9e7c540a8d32128b2794886bcbd
m22pi7 <manujm007@yahoo.com>
2023-09-09 23:12:23 -0400

    Main page input fields and buttons for station status

--- webpage.py
+++ webpage.py
@@ -2,7 +2,31 @@ from taipy.gui import Gui, navigate
 
 
 root_md="<|menu|label=Menu|lov={[('Page-1', 'Page 1'), ('Page-2', 'Page 2')]}|on_action=on_menu|>"
-page1_md="## This is page 1"
+enter_details = "Enter any details here..."
+page1_md= """
+
+# Welcome to Where's My Water?
+
+<|{"Wharton Campus.png"}|image|width="1000px"|height="1000px"|>
+
+## Add a Fountain
+
+### Notes ### {: .text-align:center;}
+
+<|{enter_details}|input|>
+#
+<|Submit|button|>
+
+### Status ###
+
+<|Working!|button|>
+<|Not Functional|button|>
+<|Not Clean|button|>
+"""
+
+
+
+
 page2_md="## This is page 4"
 
 
