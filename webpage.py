5a329551446d12916b7836b20dbe99f210d5db98
Mudit Marwaha <muditm006@gmail.com>
2023-09-09 23:09:52 -0400

    Final Touches on Favicon and Tab Name, utilized Taipy to format the .git function

--- webpage.py
+++ webpage.py
@@ -1,9 +1,13 @@
 from taipy.gui import Gui, navigate
 
 
-root_md="<|menu|label=Menu|lov={[('Page-1', 'Page 1'), ('Page-2', 'Page 2')]}|on_action=on_menu|>"
+root_md="<|menu|label=Menu|lov={[('Page-1', 'Page 1'), ('About-Us', 'About Us')]}|on_action=on_menu|>"
 page1_md="## This is page 1"
-page2_md="## This is page 4"
+page2_md="""## About Us
+
+<|{"about_us.jpg"}|image|width=1000px|height=500px|>
+
+"""
 
 
 def on_menu(state, var_name, function_name, info):
@@ -14,7 +18,7 @@ def on_menu(state, var_name, function_name, info):
 pages = {
     "/": root_md,
     "Page-1": page1_md,
-    "Page-2": page2_md
+    "About-Us": page2_md
 }
 
-Gui(pages=pages).run(use_reloader=True)
+Gui(pages=pages).run(favicon = "Favicon Final.png", title = "Where's My Water?", use_reloader=True)
