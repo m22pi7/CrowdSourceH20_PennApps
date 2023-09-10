e25193b52a6b63411e4b3a7e8f9a57ed4cf32a5c
Mudit Marwaha <muditm006@gmail.com>
2023-09-09 21:17:58 -0400

    Created webpage template with two tabs as per insturctions give on the Taipy Documentation on the webpage

--- webpage.py
+++ webpage.py
@@ -1,3 +1,20 @@
-from taipy import Gui
+from taipy.gui import Gui, navigate
 
-Gui(page="# Getting started with *Taipy*").run(use_reloader=True)
+
+root_md="<|menu|label=Menu|lov={[('Page-1', 'Page 1'), ('Page-2', 'Page 2')]}|on_action=on_menu|>"
+page1_md="## This is page 1"
+page2_md="## This is page 2"
+
+
+def on_menu(state, var_name, function_name, info):
+    page = info['args'][0]
+    navigate(state, to=page)
+
+
+pages = {
+    "/": root_md,
+    "Page-1": page1_md,
+    "Page-2": page2_md
+}
+
+Gui(pages=pages).run()
